716e47879ffb20e30e0d26f5a6b0df32f3ce41b2
breakawayz <375831500@qq.com>
2017-09-16 16:15:49 +0800

    Create Application.java

---
+++ src/main/java/com/zhangyx/Application.java
@@ -0,0 +1,13 @@
+package com.zhangyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.zhangyx"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
